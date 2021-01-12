940d384f0b30cadb8c4ec02203e64a06061cdab8
BT <brianthomasjamiel@gmail.com>
2021-01-12 14:21:54 -0800

    updated chunk of code commit

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = express();
 const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING! ON PORT 3000")
+})
