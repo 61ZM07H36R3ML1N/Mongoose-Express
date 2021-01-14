96b519e99b457fb3251f3fe9b1243d0a79b99b4c
BT <brianthomasjamiel@gmail.com>
2021-01-14 11:10:07 -0800

    added path to require in models/product

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product');
+
 //MongoDB Connection
 mongoose.connect('mongodb://localhost:27017/eShop', {useNewUrlParser: true})
     .then(() => {
