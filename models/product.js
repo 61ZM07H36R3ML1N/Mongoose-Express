550fe94b4350b642721c9ee63a0861397dd4c05a
BT <brianthomasjamiel@gmail.com>
2021-01-14 10:46:31 -0800

    created schema for MongoDB

---
+++ models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        enum: ['fruit', 'vegetable', 'dairy', 'snacks']
+    },
+})
