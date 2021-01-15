f63b70e29d6a6b69c9936f0227cdda491dc2d1c5
BT <brianthomasjamiel@gmail.com>
2021-01-15 12:21:25 -0800

    commented out single seed to import more seed data

--- seeds.js
+++ seeds.js
@@ -11,14 +11,38 @@ mongoose.connect('mongodb://localhost:27017/eShop', {useNewUrlParser: true})
         console.log(err)
     })
 
-    const p = new Product({
-        name: 'Ruby Grapefruit',
-        price: 1.99,
+    //const p = new Product({
+        //name: 'Ruby Grapefruit',
+        //price: 1.99,
+        //category: 'fruit'
+    //})
+    //p.save().then(p => {
+        //console.log(p)
+    //})
+        //.catch(e => {
+        //    console.log(e)
+        //})
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Peach',
+        price: 2.00,
         category: 'fruit'
-    })
-    p.save().then(p => {
-        console.log(p)
-    })
-        .catch(e => {
-            console.log(e)
-        })
+    },
+    {
+        name: 'Milk',
+        price: 3.00,
+        category: 'dairy'
+    },
+    {
+        name: 'Spicy Tortilla Chips',
+        price: 2.50,
+        category: 'snack'
+    }
+
+]        
