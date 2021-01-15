a5c041314916423219dee41f18ac47ef09053812
BT <brianthomasjamiel@gmail.com>
2021-01-15 10:18:25 -0800

    added lowercase in category equaling true

--- models/product.js
+++ models/product.js
@@ -13,6 +13,7 @@ const productSchema = mongoose.Schema({
     },
     category: {
         type: String,
+        lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy', 'snacks']
     },
 })
