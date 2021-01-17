b0eba3ecf3ba8d9d1a937dfb5e81009087d36645
BT <brianthomasjamiel@gmail.com>
2021-01-17 15:14:13 -0800

    code commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/products', async (req, res) => {
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
-    res.render('products/show')
+    res.render('products/show', { product })
 }) 
 
 //server setup
