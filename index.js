223be2bf6ae037868173c96f9ef8343b9f6dedfe
BT <brianthomasjamiel@gmail.com>
2021-01-25 12:09:45 -0800

    adding delete route

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ app.put('/products/:id', async (req, res) => {
     res.redirect('/products/${product._id}')
 })
 
+app.delete('/products/:id', async (req, res) =>{
+    const { id } = req.params;
+})
+
 //server setup
 app.listen(3000, () => {
     console.log("APP IS LISTENING! ON PORT 3000")
